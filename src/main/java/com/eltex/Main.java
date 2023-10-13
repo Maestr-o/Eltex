1bf2dd166b3349ecc3e4edf51d1661dfdd0e03db
Evgeniy <evg54@internet.ru>
2023-10-13 22:31:36 +0700

    Task 6

--- src/main/java/com/eltex/Main.java
+++ src/main/java/com/eltex/Main.java
@@ -1,7 +1,44 @@
 package com.eltex;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("First App!");
+        final var discount = 0.02;
+        final var discountStart = 3_000;
+        final Scanner scanner = new Scanner(System.in);
+        String currencySymbol = "";
+        int currency;
+
+        while (currencySymbol.isEmpty()) {
+            System.out.println("Введите валюту: 1 – RUB, 2 – BYN, 3 – KZT");
+            currency = scanner.nextInt();
+            if (currency == 1) {
+                currencySymbol = "₽";
+                break;
+            } else if (currency == 2) {
+                currencySymbol = "BYN";
+                break;
+            } else if (currency == 3) {
+                currencySymbol = "₸";
+            } else {
+                System.out.println("Неверный ввод, попробуйте еще раз");
+            }
+        }
+
+        System.out.println("Введите сумму покупок за прошлый год:");
+        final var clientYearlyPurchases = scanner.nextInt();
+        final double totalDiscount;
+
+        if (clientYearlyPurchases > discountStart) {
+            totalDiscount = clientYearlyPurchases * discount;
+        } else {
+            totalDiscount = 0;
+        }
+
+        if (totalDiscount > 0) {
+            System.out.printf("Попробуйте нашу новую подписку и сэкономьте %.0f%%\n", discount * 100);
+        }
+        System.out.printf("За прошлый год вы бы сэкономили с подпиской %s %s\n", totalDiscount, currencySymbol);
     }
 }
